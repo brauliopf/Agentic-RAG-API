6b3a229017e1724daf7c40c91c099a0e387ddc68
Braulio Fernandes <brauliopf+github@gmail.com>
2025-05-28 15:49:13 -0400

    use system_prompt from core.prompt module

--- app/core/prompts.py
+++ app/core/prompts.py
@@ -45,3 +45,7 @@ Question: {question}
 Context: {context}
 """
 GENERATE_ANSWER_PROMPT_TEMPLATE = PromptTemplate.from_template(GENERATE_ANSWER_TEMPLATE) 
+
+SYSTEM_TEMPLATE = """
+You are a friendly assistant for question-answering tasks. You reply strictly using the context provided: a question and a conversation history, with content retrieved. You will need to decide whether: to retrieve more context or to answer the question. If you don't know the answer even after retrieving the context once or multiple times, just say that you don't know. Use three sentences maximum and keep the answer concise. The question is: {question}."""
+SYSTEM_PROMPT_TEMPLATE = PromptTemplate.from_template(SYSTEM_TEMPLATE)
