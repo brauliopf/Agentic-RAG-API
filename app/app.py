3e8613df5a3926f9d1befba87f5ed76992f668ad
Braulio Fernandes <brauliopf+github@gmail.com>
2025-05-28 14:18:11 -0400

    create gradio interface --testing with usetako URLs

---
+++ app/app.py
@@ -0,0 +1,28 @@
+from .services.rag_service_agentic import rag_service_agentic
+from .services.document_service import document_service
+from typing import Optional, Literal
+import asyncio
+import gradio as gr
+
+async def query(question: str, max_docs: Optional[int] = None, section_filter: Optional[Literal["beginning", "middle", "end"]] = None, thread_id: Optional[str] = None):
+    return await rag_service_agentic.query(question, max_docs, section_filter, thread_id)
+
+def main(input):
+    result = asyncio.run(query(input))
+    return result["answer"]
+
+demo = gr.Interface(
+    fn=main,
+    inputs=["text"],
+    outputs=["text"],
+)
+
+if __name__ == "__main__":
+    docs = [
+        "https://www.usetako.com/termos-de-uso",
+        "https://www.usetako.com/politica-de-privacidade",
+    ]
+    for doc in docs:
+        asyncio.run(document_service.ingest_url_tako(doc))
+    
+    demo.launch(share=True)
